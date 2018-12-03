6dc6390e0745a25a90cc231301d6bd7a22af667f
angiesk <shivangi2197@hotmail.com>
2018-12-03 12:21:18 -0800

    completed first 2 task of learnyounode

---
+++ node-stuff/beginner-stuff/p1.js
@@ -0,0 +1,12 @@
+//console.log("HELLO WORLD");
+//console.log(process.argv);
+//console.log(process.argv);
+//console.log(process.argv.length);
+args=process.argv;
+//console.log("args:"+args)
+sum=0;
+for(i=2;i<args.length-2;i++)
+{
+    sum+=Number(args[i]);
+}
+console.log(sum);
