02226623a8b171888564a3c0815f7b07deacecfd
angiesk <shivangi2197@hotmail.com>
2018-12-04 05:42:17 -0800

    First Server Side code

---
+++ node-stuff/beginner-stuff/http_mod1.js
@@ -0,0 +1,8 @@
+//Importing the node htpp module
+var http = require('http');
+//Writing Server Side code
+//creating serer using the func in http module
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Echoing stuff on web browser like php');
+}).listen(9000); 
