4f54ef91be1c0c74e6d0d409f2f29373d8247ce5
angiesk <shivangi2197@hotmail.com>
2018-12-04 06:31:43 -0800

    structuring the exports

--- node-stuff/beginner-stuff/custom_mod.js
+++ node-stuff/beginner-stuff/custom_mod.js
@@ -1,11 +1,20 @@
 exports.mycustomDateTime = function () {
     return Date();
-}; 
-exports.mySumMod = function sum(a, b)
-{
-    return(a+b);    
 };
-exports.myMultMod = function mul( a,  b)
-{
-    return(a*b);    
+exports.mySumMod = function sum(a, b) {
+    return (a + b);
 };
+exports.myMultMod = function mul(a, b) {
+    return (a * b);
+};
+//Structuring the export of the custom modules
+var export_mod = module.exports = {};
+export_mod.calculations = function sub(a, b) {
+    return a - b;
+};
+export_mod.calculations = function div(a, b) {
+    if (b == 0)
+        return ("divide by zero error");
+    else
+        return (a / b);
+}
