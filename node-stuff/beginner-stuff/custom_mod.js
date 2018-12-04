f4658b0ec577048203a1e13af9ca302f7ca8e53d
angiesk <shivangi2197@hotmail.com>
2018-12-04 06:00:35 -0800

    fixing the var error in func params

--- node-stuff/beginner-stuff/custom_mod.js
+++ node-stuff/beginner-stuff/custom_mod.js
@@ -1,11 +1,11 @@
 exports.mycustomDateTime = function () {
     return Date();
 }; 
-exports.mySumMod = function sum(var a, var b)
+exports.mySumMod = function sum(a, b)
 {
     return(a+b);    
-}
-exports.myMultMod = function mul(var a, var b)
+};
+exports.myMultMod = function mul( a,  b)
 {
     return(a*b);    
-}
+};
