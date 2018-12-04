c3cb086ffa927f6975dbffec19c3280f2f93b32a
angiesk <shivangi2197@hotmail.com>
2018-12-04 05:54:24 -0800

    Creating custom  multiple node modules

---
+++ node-stuff/beginner-stuff/custom_mod.js
@@ -0,0 +1,11 @@
+exports.mycustomDateTime = function () {
+    return Date();
+}; 
+exports.mySumMod = function sum(var a, var b)
+{
+    return(a+b);    
+}
+exports.myMultMod = function mul(var a, var b)
+{
+    return(a*b);    
+}
